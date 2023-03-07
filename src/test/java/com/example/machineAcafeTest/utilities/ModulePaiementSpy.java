50a610701286c8ef76958cac311f8650788ff0e0
chadbrb1 <chad.barbier@epsi.fr>
2023-03-07 17:40:27 +0100

    Changes by Schaigne

---
+++ src/test/java/com/example/machineAcafeTest/utilities/ModulePaiementSpy.java
@@ -0,0 +1,27 @@
+package com.example.machineAcafeTest.utilities;
+
+
+import com.example.machineAcafeTest.interop.ModulePrelevementAutomatique;
+
+import java.util.ArrayList;
+
+public class ModulePaiementSpy implements ModulePrelevementAutomatique {
+    private final ModulePrelevementAutomatique _spied;
+
+    private final ArrayList<Double> _invocationsPrelever = new ArrayList<>();
+
+    public ModulePaiementSpy(ModulePrelevementAutomatique spied) {
+        _spied = spied;
+    }
+
+    @Override
+    public boolean Prelever(double montantEnEuros) {
+        var response = _spied.Prelever(montantEnEuros);
+        _invocationsPrelever.add(montantEnEuros);
+        return response;
+    }
+
+    public boolean AEteAppeleAvecLeMontant(double montant) {
+        return _invocationsPrelever.contains(montant);
+    }
+}
