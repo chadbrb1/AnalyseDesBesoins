72a1200fc0dab22e939c0f62c3b4053492e7b473
chadbrb1 <chad.barbier@epsi.fr>
2023-03-07 17:40:38 +0100

    Changes by Rémy

---
+++ src/test/java/com/example/machineAcafeTest/PaiementCarteTest.java
@@ -0,0 +1,110 @@
+package com.example.machineAcafeTest;
+
+import com.example.machineAcafeTest.utilities.MachineBuilder;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class PaiementCarteTest {
+//
+//    @Test
+//    @DisplayName("ETANT DONNE une machine reliée à un module sans-contact répondant toujours ok" +
+//            "QUAND on appuie sur le bouton CB " +
+//            "ALORS le module est interrogé " +
+//            "ET un café coule")
+//    public void TestCafeCB(){
+//        //ETANT DONNE une machine reliée à un module sans-contact répondant toujours ok
+//        var modulePaiement = new ModulePaiementSpy(ModulePaiementMock(true));
+//
+//
+//        var machine = new MachineBuilder()
+//                .AyantUnModuleDePaiement(modulePaiement)
+//                .Build();
+//
+//        int nombreCafeInitiaux = machine.GetNombreCafesServis();
+//        // QUAND une carte est détectée
+//        machine.PayerEnCB();
+//
+//        // ALORS le module est interrogé
+//        assertTrue(modulePaiement.AEteAppeleAvecLeMontant(0.4));
+//
+//        // ET un café coule
+//        int nombreCafésFinaux = machine.GetNombreCafesServis();
+//        assertEquals(nombreCafeInitiaux + 1, nombreCafésFinaux);
+//    }
+//}
+
+import com.example.machineAcafeTest.utilities.ModulePaiementMock;
+import com.example.machineAcafeTest.utilities.ModulePaiementSpy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PaiementCarteTest {
+    @Test
+    @DisplayName("ETANT DONNE une machine reliée à un module sans-contact répondant toujours ok" +
+            "QUAND on appuie sur le bouton CB " +
+            "ALORS le module est interrogé " +
+            "ET un café coule" +
+            "ET 40cts sont encaissés")
+    public void TestCaféCB(){
+        // ETANT DONNE une machine reliée à un module sans-contact répondant toujours ok
+        var modulePaiement = new ModulePaiementSpy(new ModulePaiementMock(true));
+
+        var machine = new MachineBuilder()
+                .AyantUnModuleDePaiement(modulePaiement)
+                .Build();
+
+        double argentEncaisséInitial = machine.GetArgentEncaissé();
+        int nombreCaféInitiaux = machine.GetNombreCafésServis();
+
+        // QUAND on appuie sur le bouton CB
+        machine.PayerEnCB();
+
+        // ALORS le module est interrogé
+        assertTrue(modulePaiement.AEteAppeleAvecLeMontant(0.4));
+
+        // ET un café coule
+        int nombreCafésFinaux = machine.GetNombreCafésServis();
+        assertEquals(nombreCaféInitiaux + 1, nombreCafésFinaux);
+
+        // ET 40cts sont encaissés
+        double argentEncaisséFinal = machine.GetArgentEncaissé();
+        assertEquals(argentEncaisséInitial + 0.40, argentEncaisséFinal);
+    }
+
+    @Test
+    @DisplayName("ETANT DONNE une machine reliée à un module sans-contact répondant toujours ko" +
+            "QUAND on appuie sur le bouton CB " +
+            "ALORS le module est interrogé " +
+            "ET aucun café ne coule" +
+            "ET aucun argent n'est encaissé")
+    public void TestCaféCB_Ko(){
+        // ETANT DONNE une machine reliée à un module sans-contact répondant toujours ko
+        var modulePaiement = new ModulePaiementSpy(new ModulePaiementMock(false));
+
+        var machine = new MachineBuilder()
+                .AyantUnModuleDePaiement(modulePaiement)
+                .Build();
+
+        double argentEncaisséInitial = machine.GetArgentEncaissé();
+        int nombreCaféInitiaux = machine.GetNombreCafésServis();
+
+        // QUAND on appuie sur le bouton CB
+        machine.PayerEnCB();
+
+        // ALORS le module est interrogé
+        assertTrue(modulePaiement.AEteAppeleAvecLeMontant(0.4));
+
+        // ET aucun café ne coule
+        int nombreCafésFinaux = machine.GetNombreCafésServis();
+        assertEquals(nombreCaféInitiaux, nombreCafésFinaux);
+
+        // ET aucun argent n'est encaissé
+        double argentEncaisséFinal = machine.GetArgentEncaissé();
+        assertEquals(argentEncaisséInitial, argentEncaisséFinal);
+    }
+}
